459c90e5e476cbf0d8bc859f0caca3d07a3f628f
Pavel <73703906+meow-or@users.noreply.github.com>
2022-07-25 03:36:51 +0500

    first commit

---
+++ const.js
@@ -0,0 +1,19 @@
+const commands = `
+/start - Перезапустить бота
+/help - Помощь
+/course - чо то
+`
+
+
+
+const text0 = `ipsum dolorem sit amet`
+const text1 = `dolor amet`
+const text2 = `sit amet`
+const text3 = `dolorem sit`
+
+
+module.exports.commands = commands
+module.exports.text0 = text0
+module.exports.text0 = text1
+module.exports.text0 = text2
+module.exports.text0 = text3
---
+++ index.js
@@ -0,0 +1,55 @@
+const { Telegraf, Markup } = require('telegraf')
+require('dotenv').config();
+const commands = require('./const')
+const text0 = require('./const')
+const text1 = require('./const')
+const text2 = require('./const')
+const text3 = require('./const')
+
+const bot = new Telegraf(process.env.BOTOKEN)
+bot.start((ctx) => ctx.reply(`Спящий пробудился! Чего изволишь, ${ctx.message.from.first_name ? ctx.message.from.first_name : 'кожаный'} ?!`))
+bot.help((ctx) => ctx.reply(commands))
+
+bot.command('course', async (ctx) => {
+  try {
+      await ctx.replyWithHTML('<b>DozoR</b>', Markup.inlineKeyboard(
+        [
+          [Markup.button.callback('Редакторы', 'btn_1'), Markup.button.callback('Обзоры', 'btn_2')], [Markup.button.callback('3', 'btn_3'), Markup.button.callback('4', 'btn_4')]
+        ]
+      ))
+    } 
+  catch (e) {
+      console.error(e);
+    }
+})
+
+
+function addActionBot(name, src, text) {
+  bot.action(name, async (ctx) => {
+    try {
+      await ctx.answerCbQuery()
+      if(src !== false) {
+        await ctx.replyWithPhoto({
+          source: src
+        })
+      }
+      await ctx.replyWithHTML(text, {
+        disable_web_page_preview: true
+      })
+    } catch (e) {
+      console.error(e)
+    }
+  })
+}
+
+addActionBot('btn_1', './img/1.jpg', text0)
+addActionBot('btn_2', './img/2.jpg', text1)
+addActionBot('btn_3', './img/3.jpg', text2)
+addActionBot('btn_4', false, text3)
+
+bot.launch()
+
+
+// Enable graceful stop
+process.once('SIGINT', () => bot.stop('SIGINT'))
+process.once('SIGTERM', () => bot.stop('SIGTERM'))
